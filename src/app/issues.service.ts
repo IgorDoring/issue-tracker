b4d08b29ef3a6ff0b1633df6920b275f4189a52f
IgorDoring <idoring@tutanota.com>
2024-04-07 06:21:33 -0300

    creating issue-list and issueService and mock

--- src/app/issues.service.ts
+++ src/app/issues.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Issue } from './issues';
+import { issues } from '../assets/mock-issues';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IssuesService {
 
-  private issues: Issue[] = [];
+  private issues: Issue[] = issues;
 
   constructor() { }
 
   getPendingIssues(): Issue[] {
     return this.issues.filter(issue => !issue.completed);
   }
+  
 }
