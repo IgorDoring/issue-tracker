f8939c038672f03457a409ee98354c7acef50a57
IgorDoring <idoring@tutanota.com>
2026-05-25 16:24:30 -0300

    fix: improving routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core'
 import { Routes, RouterModule } from '@angular/router'
 import { LoginFormComponent } from './login-form/login-form.component'
 import { IssueListComponent } from './issue-list/issue-list.component'
+import { authGuard } from './auth.guard'
 
 const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginFormComponent },
-  { path: 'issue-tracker', component: IssueListComponent }
+  { path: 'issues', component: IssueListComponent, canActivate: [authGuard] }
 ]
 
 @NgModule({
